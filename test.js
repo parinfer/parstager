e235e221419798151f1f67f12f1b5ab4d9187d89
shaun <shaunewilliams@gmail.com>
2023-04-19 21:37:31 -0500

    update sandbox test

--- test.js
+++ test.js
@@ -1,15 +1,35 @@
-import { restoreText } from './main.js'
+import { restoreText, createRestoreLookup } from './main.js'
 
-const { restoreCount, finalText } = restoreText(
+const a =
 `
 (foo
   )
+
+(bar (baz))
+
+(foo
+  (+ 1 2)
+  ; hello world
+  )
 `
-  ,
+
+console.log(createRestoreLookup(a))
+
+const b =
 `
 (foo)
   
-`)
+
+(comment
+  "stuff wasnt here before")
+
+(foo
+  (+ 1 2))
+  ; hello world
+  
+`
+
+const { restoreCount, finalText } = restoreText(a,b)
 
 console.log({restoreCount})
 console.log(finalText)
